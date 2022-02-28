d83dc7ff8ccd7dc6d28792bbe1ff91b96ef4d656
caterinatosarelli <99341658+caterinatosarelli@users.noreply.github.com>
2022-02-28 18:06:30 +0100

    Update Clickfind.py

--- Clickfind.py
+++ Clickfind.py
@@ -7,7 +7,17 @@ import matplotlib.pyplot as plt
 # to interact with the figure you need to set:
 # tools/preferences/graphics to be QT5 not inline
 
-def findY(f):
+def findY(f,ax,matrix):
+    
+    """This function allows to choose the peak to be fitted by clicking on its 
+    sides.
+    
+    Parameters:
+        f: figure to be clicked on
+        ax: axis along which the points will be picked
+        matrix: 
+    """
+    print('open figure and click WIDELY on either side of peak of interest')
     
     def pickY(event):
         ymouse = event.ydata # takes y coordinate of the point clicked
@@ -18,18 +28,35 @@ def findY(f):
         return times
     
     times=[]
-    print('open figure and click WIDELY on either side of peak of interest')
+    
     cid=f.canvas.mpl_connect('button_press_event',pickY)
     while len(times)<2:
         plt.pause(5)
 
     print('limits', times)
+    
+    ZLimLow = np.min(times)
+    ZLimHi = np.max(times)
+    ZLim1=np.where(ax<=ZLimLow)[0][-1] 
+    ZLim2=np.where(ax>=ZLimHi)[0][0] 
    
-    return times
+    Zoom_matrix = matrix[ZLim1:ZLim2,:]
+    
+    return Zoom_matrix, ZLim1, ZLim2
 
-def findX(f):
+def findX(f,ax):
 
     def pickX(event):
+        
+        """This function allows to select the voltage range on which we want to 
+        perform the fit by picking two points on the figure.
+        
+        Parameters:
+            f : figure on which point are picked
+            
+        Returns: 
+            voltage range limits
+        """
         xmouse = event.xdata #takes x coordinate of the point clicked
         print ('x of mouse: {:.2f}'.format(xmouse))
         times.append(xmouse)
@@ -44,5 +71,10 @@ def findX(f):
         plt.pause(5)
 
     print('limits', times)
-   
-    return times
+
+    VLim1 = np.min(times)
+    VLim2 = np.max(times)
+    VLimLow = np.where(ax<=VLim1)[0][-1] 
+    VLimHi = np.where(ax>=VLim2)[0][0] 
+
+    return VLimLow, VLimHi
